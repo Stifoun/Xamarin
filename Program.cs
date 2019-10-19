27cdc5e5ed85efc894ba8c30fe74be4ffa75eb86
Stéphane Tobola <stephane.tobola@outlook.com>
2019-10-19 13:11:43 +0200

    OK

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Jeu_de_math
+{
+    class Program
+    {
+        const int MIN = 1;
+        const int MAX = 10;
+
+        private static void DemanderAddition()
+        {
+            Random random = new Random();
+            int a = random.Next(MIN, MAX);
+            int b = random.Next(MIN, MAX);
+
+            Console.Write(a + " + " + b + " = ");
+            String reponse = Console.ReadLine();
+            int responseNum = 0;
+
+            if (int.TryParse(reponse, out responseNum))
+            {
+                if (responseNum == (a + b))
+                {
+                    Console.WriteLine("Bonne réponse");
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("ATTENTION: vous devez entrer une valeur numérique");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DemanderAddition();
+        }
+    }
+}
