7fae0cf04eb4d44b46dafd1b2db0c7eee8189792
Stéphane Tobola <stephane.tobola@outlook.com>
2019-10-20 10:11:02 +0200

    Correction de l'exercice 2

--- Program.cs
+++ Program.cs
@@ -7,38 +7,74 @@ namespace Jeu_de_math
         const int MIN = 1;
         const int MAX = 10;
 
-        private static void DemanderAddition()
+        private static bool DemanderAddition()
         {
             Random random = new Random();
             int a = random.Next(MIN, MAX);
             int b = random.Next(MIN, MAX);
 
-            Console.Write(a + " + " + b + " = ");
-            String reponse = Console.ReadLine();
-            int responseNum = 0;
-
-            if (int.TryParse(reponse, out responseNum))
+            while (true)
             {
-                if (responseNum == (a + b))
+                Console.Write(a + " + " + b + " = ");
+                String reponse = Console.ReadLine();
+                int responseNum = 0;
+
+                if (int.TryParse(reponse, out responseNum))
                 {
-                    Console.WriteLine("Bonne réponse");
+                    if (responseNum == (a + b))
+                    {
+                        Console.WriteLine("Bonne réponse");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mauvaise réponse");
+                        return false;
+                    }
+
                 }
+
                 else
                 {
-                    Console.WriteLine("Mauvaise réponse");
+                    Console.WriteLine("ATTENTION: vous devez entrer une valeur numérique");
                 }
-
-            }
-            //sinon
-            else
-            {
-                Console.WriteLine("ATTENTION: vous devez entrer une valeur numérique");
             }
         }
 
         static void Main(string[] args)
         {
-            DemanderAddition();
+            const int NB_QUESTIONS = 5;
+            int nbPoints = 0;
+            for (int i=0;i < NB_QUESTIONS; i++)
+            {
+                Console.WriteLine("Question " + (i+1) + "/" + NB_QUESTIONS);
+                if (DemanderAddition()){
+                    nbPoints++;
+                }
+                else
+                {
+
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Vous avez " + nbPoints + " sur " + NB_QUESTIONS);
+
+            if (nbPoints == NB_QUESTIONS)
+            {
+                Console.WriteLine("Excellent !");
+            }
+            else if (nbPoints == 0)
+            {
+                Console.WriteLine("Revisez vos maths !");
+            }
+            else if (nbPoints <= (NB_QUESTIONS / 2))
+            {
+                Console.WriteLine("Peut mieux faire !");
+            }
+            else
+            {
+                Console.WriteLine("Pas mal !");
+            }
         }
     }
 }
